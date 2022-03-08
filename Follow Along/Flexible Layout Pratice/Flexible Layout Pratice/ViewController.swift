69339e964b5d592ed25d4d943dcd25ab2460bea8
Jingkun Liu <liuj9@rose-hulman.edu>
2022-03-08 08:50:20 -0500

    flexible layout practice

---
+++ Follow Along/Flexible Layout Pratice/Flexible Layout Pratice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flexible Layout Pratice
+//
+//  Created by Jingkun Liu on 3/8/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
