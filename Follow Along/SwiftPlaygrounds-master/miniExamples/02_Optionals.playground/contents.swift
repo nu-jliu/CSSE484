089afc836f978efdc7bcd27cf6a2ae26a99114b8
Jingkun Liu <liuj9@rose-hulman.edu>
2022-03-11 16:02:13 -0500

    optional

--- Follow Along/SwiftPlaygrounds-master/miniExamples/02_Optionals.playground/contents.swift
+++ Follow Along/SwiftPlaygrounds-master/miniExamples/02_Optionals.playground/contents.swift
@@ -6,39 +6,54 @@ let dave = "Dave"
 
 // Part 1.
 // Optionals
+var optionalFloat: Float?
+var requiredFloat: Float
+var implicitlyUnwrappedFloat: Float!
 
+optionalFloat = 5.0
+requiredFloat = 5.0
 
-
-
+print("Optioal Float = \(optionalFloat)")
+print("Required Float = \(requiredFloat)")
 
 // Optionals with forced unwrapping
 
+print("Optional Float = \(optionalFloat)")
 
+let name = "Dave"
+let number = "7"
+var optioalThatIsNill: Int? = Int(name)
+var optionalThisIsNotNill: Int = Int(number)!
 
-
-
-
-
-
-
+optioalThatIsNill
+optionalThisIsNotNill
 
 // Part 2.
+import UIKit
+import Foundation
 // Views in a Playground + Optional Chaining
+let b = UIButton(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+b.backgroundColor = UIColor.red
 
+b.setTitle("Press Me!!", for: .normal)
 
+print(b.titleLabel)
+print(b.titleLabel?.text) // optional printing
+print(b.titleLabel!.text) // required printing
 
-
-
-
-
+print(b.titleLabel!.text!)
 
 // Optional Binding
 
-
-
-
-
-
+if let myText = b.titleLabel?.text {
+    print("There is text and the value is \(myText)")
+} else {
+    print("There is no text on the label")
+}
 
 
 // Implicitly Unwrapped Optionals
+let myImplicitlyUnrappedOptionalLabel: UILabel! = b.titleLabel
+
+print(myImplicitlyUnrappedOptionalLabel.text)
+print(myImplicitlyUnrappedOptionalLabel.text!)
--- Follow Along/SwiftPlaygrounds-master/miniExamples/03_Collections.playground/contents.swift
+++ Follow Along/SwiftPlaygrounds-master/miniExamples/03_Collections.playground/contents.swift
@@ -2,23 +2,30 @@ import UIKit
 
 // Basics for arrays
 var names = ["Dave", "Kristy", "McKinley", "Keegan", "Bowen", "Neala"]
+names[1]
+names[2] = "Hello"
+names.count
 
-
-
-
-
-
-
-
-
+if names.contains("Dave") {
+    print("Yes, it is present")
+}
 
 // Appending values
+var ages = [Int]()
+ages.append(37)
+ages.append(4)
+ages.insert(10, at: 0)
 
+ages
 
+ages += [1, 3, 7]
+ages
 
-
-
-
+if ages.isEmpty {
+    print("Yes it is empty")
+} else {
+    print("Array has \(ages.count) elements")
+}
 
 
 
@@ -33,9 +40,15 @@ for _ in 0..<100 {
 var teenagers = randomAges.filter({
     return $0 > 12 && $0 < 20
 })
-teenagers.sorted(by: {
+teenagers = teenagers.sorted(by: {
     return $0 < $1
 })
+
+teenagers = teenagers.map({
+    return $0 + 100
+})
+
+teenagers
 
 
 // Dictionaries
@@ -54,3 +67,4 @@ var tuple = ("item1", "item2", "item3", "item4")
 tuple.2
 var namedTuple = (first : "Dave", last : "Fisher")
 namedTuple.last
+namedTuple.first
