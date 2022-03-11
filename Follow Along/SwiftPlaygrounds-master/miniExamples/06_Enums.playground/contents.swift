8a272d7d3b4b785dd670fac5e278091bdc4a6e6a
Jingkun Liu <liuj9@rose-hulman.edu>
2022-03-11 17:04:42 -0500

    enum

--- Follow Along/SwiftPlaygrounds-master/miniExamples/06_Enums.playground/contents.swift
+++ Follow Along/SwiftPlaygrounds-master/miniExamples/06_Enums.playground/contents.swift
@@ -1,16 +1,50 @@
 // Basic enum
+enum WeekDays {
+    case monday
+    case tuesday
+    case wednesday
+    case thursday
+    case friday
+}
+
+var today: WeekDays
+today = WeekDays.monday
+
+if today == WeekDays.monday {
+    print("It is monday")
+}
 
 
-
-
-
+switch today {
+case .monday, .wednesday, .friday:
+    print("swim practice")
+case .thursday:
+    print("Horse back riding")
+case .tuesday:
+    print("Piano practice")
+default:
+    print("No schedule")
+//    break
+}
 
 
 
 
 // Rawvalues
 
+enum State: Int {
+    case ready = 0
+    case set = 1
+    case go = 2
+}
 
+var raceState = State.ready
+raceState.rawValue
+
+var nextRaceState = State(rawValue: 0)
+if nextRaceState?.rawValue == 0 {
+    print("Ready")
+}
 
 
 
@@ -23,5 +57,25 @@
 
 
 // Associated values and functions
+enum HomeworkStatus {
+    case noHomework
+    case inProgress(Int, Int)
+    case done
+    
+    func simpleDescription() -> String {
+        switch self {
+        case .noHomework:
+            return "No Homework"
+        case .inProgress(let workDone, let totalProblems):
+            return "You have finished \(workDone) out of \(totalProblems) problems"
+        case .done:
+            return "Done"
+        }
+    }
+}
+
+var myHomeworkStatus = HomeworkStatus.inProgress(8, 10)
+myHomeworkStatus.simpleDescription()
+
 
 
