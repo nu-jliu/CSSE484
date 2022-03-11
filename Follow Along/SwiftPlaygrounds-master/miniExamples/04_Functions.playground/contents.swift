c1c9ac8d62c5693d98f672d6c77b5ba701d558ef
Jingkun Liu <liuj9@rose-hulman.edu>
2022-03-11 16:18:13 -0500

    functions

--- Follow Along/SwiftPlaygrounds-master/miniExamples/04_Functions.playground/contents.swift
+++ Follow Along/SwiftPlaygrounds-master/miniExamples/04_Functions.playground/contents.swift
@@ -1,12 +1,15 @@
 // Basic function
 
+func stringRepeater(_ orgStr: String, _ repeatCount: Int) -> String {
+    var returnStr = ""
+    for _ in 0..<repeatCount {
+        returnStr += orgStr
+    }
+    return returnStr
+}
 
-
-
-
-
-//stringRepeater("Ho! ", 3)
-//stringRepeater("New York ", 2)
+stringRepeater("Ho!", 3)
+stringRepeater("New York ", 2)
 
 
 
@@ -14,14 +17,24 @@
 // Returning a tuple (multiple return values)
 
 
+func stringLetterRepeater(_ orgStr: String, _ repeatCount: Int) -> (String, String) {
+    let version1 = stringRepeater(orgStr, repeatCount)
+    
+    var version2 = ""
+    
+    for letter in orgStr {
+        version2 += String(repeating: letter, count: repeatCount)
+    }
+    
+    return (version1, version2)
+}
 
 
 
-
-//var collated : String, uncollated : String
-//(collated, uncollated) = stringLetterRepeater("Ow! ", 4)
-//collated
-//uncollated
+var collated : String, uncollated : String
+(collated, uncollated) = stringLetterRepeater("Ow! ", 4)
+collated
+uncollated
 
 
 
@@ -32,10 +45,18 @@
 // Internal/External names plus Optional parameters
 
 
+func stringDoubler(_ orgString: String, alternativeMultiple repeatCount: Int = 2) -> String {
+    var returnStr = ""
+    
+    for _ in 0..<repeatCount {
+        returnStr += orgString
+    }
+    
+    return returnStr
+}
 
 
 
 
-
-//stringDoubler("Woof! ", alternativeMultiple: 3)
-//stringDoubler("Jingle Bells! ")
+stringDoubler("Woof! ", alternativeMultiple: 3)
+stringDoubler("Jingle Bells! ")
