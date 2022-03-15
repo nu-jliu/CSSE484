078b6f567adb7a694b29f4852e5420119a630756
Jingkun Liu <liuj9@rose-hulman.edu>
2022-03-15 09:10:44 -0400

    in class

--- Follow Along/Swift Playgrounds/miniExamples/08_InClass.playground/Contents.swift
+++ Follow Along/Swift Playgrounds/miniExamples/08_InClass.playground/Contents.swift
@@ -30,6 +30,7 @@ for i in 0..<scores.count {
 totalPassing
 
 import UIKit
+import Foundation
 
 var number: String = "100"
 var dave: String = "Dave"
@@ -52,4 +53,119 @@ if let tempVariable = Int(dave) {
     print("Temp variable is \(tempVariable)")
 } else {
     print("There is no temp variable")
+}
+
+enum Weekday {
+    case monday, tuesday, wednesday, thursday, friday
+}
+
+var today: Weekday = .tuesday
+
+switch today {
+case .monday, .tuesday, .thursday:
+    print("You have classes");
+case .wednesday:
+    print("Weekend Wednesday!");
+default:
+    print("It is the weekend");
+}
+
+enum State: Int {
+    case ready = 0
+    case steady
+    case go
+}
+
+var raceState = State.go
+type(of: raceState)
+
+var nextRaceState = State(rawValue: 2)
+type(of: nextRaceState)
+nextRaceState?.rawValue
+
+if nextRaceState == .go {
+    print("Go!!!")
+}
+
+enum HomeworkStatus {
+    case noHomework
+    case inProgress(Int, Int)
+    case done
+    
+    func simpleDescription() -> String {
+        switch self {
+        case .noHomework:
+            return "No Homework"
+        case .inProgress(let numComplete, let total):
+            return "You have finished \(numComplete) out of \(total), remaining \(total - numComplete)"
+        case .done:
+            return "You finished homeworks"
+        }
+    }
+}
+
+var myHwStatus = HomeworkStatus.inProgress(6, 10)
+var myHwStatus2 = HomeworkStatus.inProgress(8, 100)
+
+myHwStatus.simpleDescription()
+myHwStatus2.simpleDescription()
+
+class BankAccount: CustomStringConvertible {
+    
+    var name: String
+    var balance: Double
+    
+    init(name: String, balance: Double) {
+        self.name = name
+        self.balance = balance
+    }
+    
+    func deposit(_ amount: Double) {
+        self.balance += amount
+    }
+    
+    func withdraw(_ amount: Double) {
+        self.balance -= amount
+    }
+    
+    var description: String {
+        get {
+            return "\(self.name): $\(self.balance)"
+        }
+    }
+}
+
+var allenAccount = BankAccount(name: "Allen", balance: 2890)
+allenAccount.deposit(200)
+allenAccount.balance
+allenAccount.withdraw(80)
+allenAccount.balance
+
+class ATMAccount: BankAccount {
+    
+    var withdrawFee: Double
+    
+    init(name: String, balance: Double, withdrawFee: Double) {
+        self.withdrawFee = withdrawFee
+        super.init(name: name, balance: balance)
+    }
+    
+    convenience init() {
+        self.init(name: "Unknown", balance: 0.0)
+    }
+    
+    override convenience init(name: String, balance: Double) {
+        self.init(name: name, balance: balance, withdrawFee: 2.0)
+    }
+    
+    override func withdraw(_ amount: Double) {
+        self.balance -= self.withdrawFee
+        super.withdraw(amount)
+    }
+    
+    override var description: String {
+        get {
+            return super.description + ", Fee: $\(self.withdrawFee)"
+        }
+    }
 }
