84d2c3539d6d819586463c4fae376713788caccc
Jingkun Liu <liuj9@rose-hulman.edu>
2022-03-15 00:28:17 -0400

    tie tac toe finished

--- Follow Along/ticTacToeGame/TicTacToeGame.playground/Contents.swift
+++ Follow Along/ticTacToeGame/TicTacToeGame.playground/Contents.swift
@@ -1,3 +1,4 @@
+import Darwin
 
 
 // TODO: Create the TicTacToeGame class
@@ -6,8 +7,8 @@ class TicTacToeGame: CustomStringConvertible {
     
     enum MarkType: String {
         case none = "-"
-        case x = "x"
-        case o = "o"
+        case x = "X"
+        case o = "O"
     }
     
     enum State: String {
@@ -32,10 +33,68 @@ class TicTacToeGame: CustomStringConvertible {
         self.state = TicTacToeGame.State.xTurn
     }
     
-    func getBoardString() -> String {
+    func pressedSquareAt(_ index: Int) {
+        if (self.board[index] != .none) {
+            print("ERROR: The square is not empty")
+            return
+        }
+        
+        switch self.state {
+        case .xWin, .oWin, .tie:
+            print("WARNING: The game is over already, no move is allowed")
+            return
+            
+        case .xTurn:
+            self.board[index] = .x
+            self.state = .oTurn
+            
+        case .oTurn:
+            self.board[index] = .o
+            self.state = .xTurn
+        }
+        
+        self.checkForWin()
+    }
+    
+    func checkForWin() {
+        // Check for tie BEFORE check for win
+        
+        if !self.board.contains(.none) {
+            self.state = .tie
+            return
+        }
+        
+        // Check for win
+        
+        var linesOfThree = [String]()
+        
+        for i in 0..<3 {
+            let startIndex = 3*i
+            linesOfThree.append(self.getBoardString([startIndex, startIndex+1, startIndex+2]))
+        }
+        
+        for i in 0..<3 {
+            linesOfThree.append(self.getBoardString([i, i+3, i+6]))
+        }
+        
+        linesOfThree.append(self.getBoardString([0, 4, 8]))
+        linesOfThree.append(self.getBoardString([2, 4, 6]))
+        
+        for lineOf3 in linesOfThree {
+            if lineOf3 == "XXX" {
+                self.state = .xWin
+                return
+            } else if lineOf3 == "OOO" {
+                self.state = .oWin
+                return
+            }
+        }
+    }
+    
+    func getBoardString(_ indices: [Int] = [0, 1, 2, 3, 4, 5, 6, 7, 8]) -> String {
         var gameStr = ""
         
-        for i in 0..<self.board.count {
+        for i in indices {
             gameStr.append(self.board[i].rawValue)
         }
         
@@ -43,32 +102,32 @@ class TicTacToeGame: CustomStringConvertible {
     }
     
     var description: String {
-        return "State: \(self.state) Board: \(self.getBoardString())"
+        return "State: \(self.state), Board: \(self.getBoardString())"
     }
 }
 
 /* ----------------- Official Playground testing ----------------- */
 var game = TicTacToeGame()
-game.board[2] = .x
-game
-//game.pressedSquareAt(0)
-//game.pressedSquareAt(1)
-//game.pressedSquareAt(3)
-//game.pressedSquareAt(2)
-//game.pressedSquareAt(6)
+//game.board[0] = .x
+//game
+game.pressedSquareAt(0) // X is moving in the upper left
+game.pressedSquareAt(1) // O is moving in the top center
+game.pressedSquareAt(3) // X is below the other X
+game.pressedSquareAt(2) // O is moving to the top right
+game.pressedSquareAt(6) // X wins the game!!!
 
 
-//var game2 = TicTacToeGame()
-//game2.board = [.x, .x, .o,
-//                .none, .none, .none,
-//                .o, .none, .none]
-//game2.pressedSquareAt(8)
-//game2.pressedSquareAt(4)
+var game2 = TicTacToeGame()
+game2.board = [.x, .x, .o,
+                .none, .none, .none,
+                .o, .none, .none]
+game2.pressedSquareAt(8)
+game2.pressedSquareAt(4)
 
 
-//var game3 = TicTacToeGame()
-//game3.board = [.x, .x, .o,
-//    .o, .o, .x,
-//    .x, .o, .none]
-//game3.pressedSquareAt(8)
+var game3 = TicTacToeGame()
+game3.board = [.x, .x, .o,
+    .o, .o, .x,
+    .x, .o, .none]
+game3.pressedSquareAt(8)
 
