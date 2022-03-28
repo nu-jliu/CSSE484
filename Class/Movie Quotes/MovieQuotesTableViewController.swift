f55d5e5afc9b9170402d7e5231a3333e38a94c57
Jingkun Liu <liuj9@rose-hulman.edu>
2022-03-28 08:44:59 -0400

    uitale controller

---
+++ Class/Movie Quotes/MovieQuotesTableViewController.swift
@@ -0,0 +1,58 @@
+//
+//  MovieQuotesTableViewController.swift
+//  Movie Quotes
+//
+//  Created by Jingkun Liu on 3/28/22.
+//
+
+import UIKit
+
+class MovieQuotesTableViewController: UITableViewController {
+    
+    let movieQuoteCell = "MovieQuoteCell"
+    let names = ["Dave", "Kristy", "McKinley", "Keegan", "Bowen", "Neala"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+    
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return self.names.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: self.movieQuoteCell, for: indexPath)
+
+        // Configure the cell...
+        cell.textLabel?.text = self.names[indexPath.row]
+
+        return cell
+    }
+
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // TODO: implement delete
+        }
+    }
+
+    
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+}
